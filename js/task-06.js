653ebd730017c01db055631798abe05d12fc3ac7
Mariia Kovalova <mashuha1698@gmail.com>
2022-12-04 23:43:24 +0200

    hw6 upg

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,9 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
-  event.currentTarget.value.length === Number(event.currentTarget.dataset.length)
-    ? event.currentTarget.classList.add('valid')
-    : event.currentTarget.classList.add('invalid');
+  if (event.currentTarget.value.length === Number(event.currentTarget.dataset.length)) {
+    event.currentTarget.classList.add('valid');
+  } else {
+    event.currentTarget.classList.add('invalid');
+  }
 }
