c5f57b1408951197060a57cff22f3bfe24fc7c84
Mariia Kovalova <mashuha1698@gmail.com>
2022-12-10 09:21:57 +0200

    tast 6 upg

--- js/task-06.js
+++ js/task-06.js
@@ -5,7 +5,9 @@ inputEl.addEventListener('blur', onInputBlur);
 function onInputBlur(event) {
   if (event.currentTarget.value.length === Number(event.currentTarget.dataset.length)) {
     event.currentTarget.classList.add('valid');
+    event.currentTarget.classList.remove('invalid');
   } else {
     event.currentTarget.classList.add('invalid');
+    event.currentTarget.classList.remove('valid');
   }
 }
